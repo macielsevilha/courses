f825c063a09f0d9d8919a5b45668c3fbb00b5d34
Maciel Sevilha <dev.macielsevilha@gmail.com>
2022-05-11 02:48:05 -0300

    finalizando a estrutura

---
+++ curso/gulp/spa/gulpTasks/servidor.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+
+function monitorarArquivos(cb) {
+
+    return cb()
+}
+
+function servidor(cb) {
+
+    return cb()
+}
+
+module.exports = {
+    monitorarArquivos,
+    servidor
+}
