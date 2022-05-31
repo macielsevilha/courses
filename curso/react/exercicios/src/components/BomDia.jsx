78e71c5ce3e476fb867e346a1ab4bf8b783b52be
Maciel Sevilha <dev.macielsevilha@gmail.com>
2022-05-31 11:54:15 -0300

    componente com prmt

---
+++ curso/react/exercicios/src/components/BomDia.jsx
@@ -0,0 +1,9 @@
+import React, {Fragment} from "react";
+
+export default props =>
+    <Fragment>
+         <h1>Bom dia jovem {props.nome}</h1>
+        <p>Resultado do curso</p>
+    </Fragment>
+
+    // Dentro de div, [] array ou fragment
--- curso/react/exercicios/src/index.js
+++ curso/react/exercicios/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro'
+import BomDia from './components/BomDia'
 
 
-ReactDOM.render(< Primeiro />, document.getElementById('root'))
+ReactDOM.render(< BomDia nome="Maciel" />, document.getElementById('root'))
