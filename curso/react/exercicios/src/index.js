f8233979a8c709f91028f7bbb60d4c624f55d2de
Maciel Sevilha <dev.macielsevilha@gmail.com>
2022-06-01 03:17:39 -0300

    múltipos componentes

--- curso/react/exercicios/src/index.js
+++ curso/react/exercicios/src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom'
 
 import BomDia from './components/BomDia'
 
+import Multi from './components/Multiplos'
 
-ReactDOM.render(< BomDia nome="Maciel" />, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana"></Multi.BoaTarde>
+        <Multi.BoaNoite nome="Maciel" />
+    </div>
+    , document.getElementById('root'))
