510ab33b9f4919b518b30af37841949246597941
Maciel Sevilha <dev.macielsevilha@gmail.com>
2022-06-09 03:03:48 -0300

    componente Conteúdo

--- curso/react/crud/frontend/src/components/template/Main.jsx
+++ curso/react/crud/frontend/src/components/template/Main.jsx
@@ -5,8 +5,10 @@ import Header from "./Header";
 export default props =>
      <React.Fragment>
          <Header {...props} />
-         <main className="content">
-             Conteúdo 
+         <main className="content container-fluid">
+             <div className="p-3 mt-3">
+                 {props.children}
+             </div>
          </main>
      </React.Fragment>
 
--- curso/react/crud/frontend/src/main/App.jsx
+++ curso/react/crud/frontend/src/main/App.jsx
@@ -5,13 +5,13 @@ import './App.css'
 
 import Logo from "../components/template/Logo";
 import Nav from "../components/template/Nav";
-import Main from "../components/template/Main";
+import Home from '../components/home/Home';
 import Footer from "../components/template/Footer";
 
 export default props =>
    <div className="app">
        <Logo />
        <Nav />
-       <Main icon="home" title="início" subtitle="Segundo Projeto do capítulo de React." />
+       <Home />
        <Footer />
    </div>
