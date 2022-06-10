bbd794c45db937640daeed844569f73e644fe609
MacielSevilha <dev.macielsevilha@gmail.com>
2022-06-10 20:54:41 -0300

    componente de cadastro

---
+++ curso/react/crud/frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Component } from "react";
+import Main from './template/Main'
+
+const headerProps = {
+    icon: 'users',
+    title: 'Usuários',
+    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
+}
+
+export default class UserCrud extends Component {
+    render() {
+        return (
+            <Main {...headerProps} >
+                Cadastro de Usuário
+            </Main>
+        )
+    }
+}
