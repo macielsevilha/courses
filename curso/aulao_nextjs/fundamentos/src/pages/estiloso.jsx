52d7402bdea5a2ce57c8dfef078d66b209f1bf42
MacielSevilha <dev.macielsevilha@gmail.com>
2022-06-17 10:34:14 -0300

    css modularizado

---
+++ curso/aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from './Estiloso.module.css'
+
+export default function Estiloso() {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS módulos</h1>
+        </div>
+    )
+}
