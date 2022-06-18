a4416e2d31837ea62a2623c54e4ff11bc98da473
MacielSevilha <dev.macielsevilha@gmail.com>
2022-06-18 08:29:50 -0300

    navegação entre componente

--- curso/aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ curso/aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,8 +1,10 @@
 import styles from '../styles/Estiloso.module.css'
+import Link from 'next/link'
 
 export default function Estiloso() {
     return (
         <div className={styles.roxo}>
+            <Link href="/">Voltar</Link>
             <h1>Estilo usando CSS módulos</h1>
         </div>
     )
--- curso/aulao_nextjs/fundamentos/src/pages/index.jsx
+++ curso/aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
+
 export default function() {
     return (
         <div>
-            <h1>fundamentos de next.js & react</h1>
-            <h2>vamos estudar esse framework</h2>
+            <Link href="/estiloso">
+                Estiloso
+            </Link>
         </div>
     )
 }
