9b53a7c54136e0871ce1dbc288e616c971342cba
MacielSevilha <dev.macielsevilha@gmail.com>
2022-06-19 19:50:12 -0300

    navegação simples

--- curso/aulao_nextjs/fundamentos/src/pages/index.jsx
+++ curso/aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -13,6 +13,8 @@ export default function() {
             <Navegador texto="Estiloso" destino="/estiloso" />
             <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3" />
             <Navegador texto="JSX" destino="/jsx" cor="crimson" />
+            <Navegador texto="Navegação #01" destino="/navegacao" cor="green" />
+
         </div>
     )
 }
---
+++ curso/aulao_nextjs/fundamentos/src/pages/navegacao/index.jsx
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout";
+
+
+export default function Navegacao() {
+    return (
+        <Layout titulo="Exemplo de navegação #01">
+            <h1>Navegação #01</h1>
+        </Layout>
+    )
+}
