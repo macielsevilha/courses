8cb8640256775c27a7dd6b98ad9773fad97a2acf
MacielSevilha <dev.macielsevilha@gmail.com>
2022-06-23 17:54:35 -0300

    componente botçao

--- curso/next-crud/src/pages/index.tsx
+++ curso/next-crud/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Botao from "../components/Botao";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -25,7 +26,10 @@ export default function Home() {
    text-white
    `}>
      <Layout titulo="Cadastro Simples">
-         <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
+      <div className="flex justify-end">
+        <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+      </div>
+        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
      </Layout>
    </div>
   )
--- curso/next-crud/tailwind.config.js
+++ curso/next-crud/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      /^bg-/,
+      /^to-/,
+      /^from-/,
+    ]
+  },
+  
   theme: {
     extend: {},
   },
