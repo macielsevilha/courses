9e5b5b2f93eb4fa247b60edf04bf46df187badb0
MacielSevilha <dev.macielsevilha@gmail.com>
2022-06-24 22:51:52 -0300

    integrando tabela e formulário

--- curso/next-crud/src/pages/index.tsx
+++ curso/next-crud/src/pages/index.tsx
@@ -7,6 +7,9 @@ import Cliente from "../core/Cliente";
 
 export default function Home() {
 
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio)
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
   const clientes = [
     new Cliente('ana', 24, '1'),
     new Cliente('Maciel', 20, '2'),
@@ -15,18 +18,23 @@ export default function Home() {
   ]
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome)
+    setCliente(cliente)
+    setVisivel('form')
   }
   function clienteExcluido(cliente: Cliente) {
     console.log('exluir ' + cliente.nome)
   }
+  function novoCliente() {
+    setCliente(Cliente.vazio)
+    setVisivel('form')
+  }
 
   function salvarCliente(cliente: Cliente) {
      console.log(cliente)
+     setVisivel('tabela')
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-
+ 
   return (
     <div className={`
    flex justify-center items-center h-screen
@@ -37,7 +45,7 @@ export default function Home() {
         {visivel === 'tabela' ? (
           <>
             <div className="flex justify-end">
-              <Botao cor="green" className="mb-4" onClick={() => setVisivel()}>
+              <Botao cor="green" className="mb-4" onClick={novoCliente}>
                 Novo Cliente
               </Botao>
             </div>
@@ -46,7 +54,7 @@ export default function Home() {
         ) : (
           <>
             <Formulario 
-            cliente={clientes[2]}
+            cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')}
             />
