7d51722a68431b8d69002fed388267672a3e0dba
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-06 18:33:34 -0300

    implementando a lógica

--- curso/vue/calculator/src/main/Calculator.vue
+++ curso/vue/calculator/src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="calculator">
-    <Display value="1000" />
+    <Display :value="displayValue" />
     <Button  label="AC" triple @onClick="clearMemory" />
     <Button label="/" operation @onClick="setOperation" />
     <Button label="7" @onClick="addDigit" />
@@ -26,19 +26,64 @@ import Display from '@/components/Display.vue'
 import Button from '@/components/Button.vue'
 export default {
   name: 'AllCaculador',
+  data: function() {
+    return {
+      displayValue: "0",
+      clearDisplay: false,
+      operation: null,
+      values: [0,0],
+      current: 0
+    }
+  },
   components: {
     Display,
     Button
   },
   methods: {
     clearMemory() {
-    
+       Object.assign(this.$data, this.$options.data())
     },
     setOperation(operation) {
-     
+     if (this.current === 0) {
+      this.operation = operation
+      this.current = 1
+      this.clearDisplay = true
+     } else {
+      const equals = operation === "="
+      const currentOperation = this.operation
+
+      try{
+        this.values[0] = eval(
+          `${this.values[0]} ${currentOperation} ${this.values[1]}`)
+        if (isNaN(this.values[0]) || !isFinite(this.values[0])) {
+	this.clearMemory()
+return
+}
+      } catch (e) {
+        this.$emit('onError', e)
+      }
+
+      this.values[1] = 0
+
+      this.displayValue = this.values[0]
+      this.operation = equals ? null : operation
+      this.current = equals ? 0 : 1
+      this.clearDisplay = !equals
+     }
     },
     addDigit(n) {
-     
+      if (n === "." && this.displayValue.includes(".")) {
+        return
+      }
+
+      const clearDisplay = this.displayValue === "0" || this.clearDisplay
+      const currentValue = clearDisplay ? "" : this.displayValue
+      const displayValue = currentValue + n
+      
+      this.displayValue = displayValue
+      this.clearDisplay = false
+      this.values[this.current] = displayValue
+
     }
   }
 }
