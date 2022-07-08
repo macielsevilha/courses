e9ee1e46c85470ff444b1f26eec3ab78dd1402bb
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-07 22:47:12 -0300

    problema de monty hall #02

--- curso/vue/montyhall/src/App.vue
+++ curso/vue/montyhall/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
+    <Gift />
   </div>
 </template>
 
 <script>
-
+import Gift from './components/Gift.vue'
 export default {
- 
+ name: 'App',
+  components: { Gift }
   
 }
 </script>
