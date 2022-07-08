beddade901c9944579ce24d8c733f3d3dfb980c1
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-08 14:14:35 -0300

    problema de monty hall #04

--- curso/vue/montyhall/src/App.vue
+++ curso/vue/montyhall/src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
-   <Door number="1" :hasGift="true" />
+    <div class="form">
+      <div v-if="!started">
+         <label for="protAmount">Quantas portas?</label>
+         <input type="text" id="portAmount" size="3" v-model.number="portsAmount">
+      </div>
+      <div v-if="!started">
+         <label for="selectedPort">Qaul porta é premiada?</label>
+         <input type="text" id="selectedPort" size="3" v-model.number="selectedPort">
+      </div>
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <Door :hasGift="i === selectedPort" :number="i" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +25,14 @@
 import Door from './components/Door.vue'
 export default {
  name: 'App',
-  components: { Door }
+  components: { Door },
+  data: function() {
+    return {
+      started: false,
+      portsAmount: 3,
+      selectedPort: null
+    }
+  }
   
 }
 </script>
@@ -33,5 +56,23 @@ export default {
     background-color: #0004;
     padding: 20px;
     margin-bottom: 60px;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+  }
+  .form, .form input, .form button {
+    margin-bottom: 10px;
+    font-size: 2rem;
+  }
+  .doors {
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+
+    flex-wrap: wrap;
   }
 </style>
