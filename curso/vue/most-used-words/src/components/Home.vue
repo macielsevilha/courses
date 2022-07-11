193455e9fd5b89f825d6a3dc75b654f8ea3d7603
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-11 17:22:35 -0300

    interface gráfica com vue JS #01

---
+++ curso/vue/most-used-words/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid>
+    <div class="pills">
+      <pill-vue v-for="word in groupedWords" :key="word.name"
+        :name="word.name" :amount="word.amount" />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import PillVue from './Pill.vue'
+
+export default {
+  name: "AllHome",
+  components: { PillVue },
+  data: function() {
+    return {
+        groupedWords: [
+            { name: 'my', amount: 1223},
+            { name: 'his', amount: 900},
+            { name: 'hir', amount: 590},
+        ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ curso/vue/most-used-words/src/components/Pill.vue
@@ -0,0 +1,17 @@
+ <template>
+   <div>
+    <span>{{ name }}</span>
+    <span>{{ amount }}</span>
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+    name: 'AllPill',
+    props: ['name', 'amount']
+ }
+ </script>
+ 
+ <style>
+ 
+ </style>
