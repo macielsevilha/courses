e55cb767cc47d7b97705cf542a905bcae7bf9f0d
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-12 19:51:38 -0300

    processando arquivos #01

--- curso/vue/most-used-words/src/assets/backend/index.js
+++ curso/vue/most-used-words/src/assets/backend/index.js
@@ -1,15 +1,21 @@
 const { ipcMain } = require('electron')
 
-ipcMain.on('process-subtitles', (event, paths) => {
-  console.log(paths)
+const pathsToRows = require('./pathToRows')
 
-  event.reply('process-subtitles', [
-    { name: 'my', amount: 1223},
-    { name: 'his', amount: 900},
-    { name: 'hir', amount: 590},
-    { name: 'its', amount: 1223},
-    { name: 'our', amount: 700},
-    { name: 'your', amount: 560},
-    { name: 'their', amount: 12},
-  ])
+ipcMain.on('process-subtitles', (event, paths) => {
+  
+  pathsToRows(paths)
+    .then(rows => console.log(rows))
+    .then(() => {
+      event.reply('process-subtitles', [
+        { name: 'my', amount: 1223},
+        { name: 'his', amount: 900},
+        { name: 'hir', amount: 590},
+        { name: 'its', amount: 1223},
+        { name: 'our', amount: 700},
+        { name: 'your', amount: 560},
+        { name: 'their', amount: 12},
+      ])
+    })
+
 })
