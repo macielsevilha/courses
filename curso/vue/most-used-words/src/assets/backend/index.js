037e6fd73fe93892b292044df2645ee15e767157
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-14 19:04:50 -0300

    backend com JSON server

--- curso/vue/most-used-words/src/assets/backend/index.js
+++ curso/vue/most-used-words/src/assets/backend/index.js
@@ -2,22 +2,14 @@ const { ipcMain } = require('electron')
 
 const pathsToRows = require('./pathToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitles', (event, paths) => {
   
   pathsToRows(paths)
     .then(rows => prepareData(rows))
-    .then(words => console.log(words))
-    .then(() => {
-      event.reply('process-subtitles', [
-        { name: 'my', amount: 1223},
-        { name: 'his', amount: 900},
-        { name: 'hir', amount: 590},
-        { name: 'its', amount: 1223},
-        { name: 'our', amount: 700},
-        { name: 'your', amount: 560},
-        { name: 'their', amount: 12},
-      ])
-    })
-
+    .then(words =>groupWords(words))
+    .then(groupedWords => event.reply('process-subtitles', groupedWords))
+   
 })
+
