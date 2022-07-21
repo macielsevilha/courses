f94ff8b8fee3747d3df31daaf4ffb57002bcf3d7
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-21 20:43:15 -0300

    diretivas na prática #01

--- curso/angular 9/crud/frontend/src/app/app.module.ts
+++ curso/angular 9/crud/frontend/src/app/app.module.ts
@@ -13,7 +13,8 @@ import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatCardModule } from '@angular/material/card'
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive'
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
