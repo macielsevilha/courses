23531f1311aa7cfc99676fb9273fcb1b96b57e8f
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-22 18:37:15 -0300

    componente criar produto

--- curso/angular 9/crud/frontend/src/app/app.module.ts
+++ curso/angular 9/crud/frontend/src/app/app.module.ts
@@ -15,7 +15,8 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
-import { ForDirective } from './directives/for.directive'
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ForDirective } from './directives/for.directive'
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    ForDirective
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
