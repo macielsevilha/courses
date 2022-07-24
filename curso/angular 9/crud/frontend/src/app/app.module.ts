046e093cdfb50369a41401bed79997f20d18d43f
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-24 19:13:50 -0300

    Componente Obter Produto

--- curso/angular 9/crud/frontend/src/app/app.module.ts
+++ curso/angular 9/crud/frontend/src/app/app.module.ts
@@ -23,7 +23,8 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { MatInputModule } from '@angular/material/input'
     ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
