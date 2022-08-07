b172cf0d770e5577b2755eab2109865ebb483998
MacielSevilha <dev.macielsevilha@gmail.com>
2022-08-07 18:08:40 -0300

    Inserir Estados ID

---
+++ umdey/programação moderna/banco-relacional/inserirNovosEstados.sql
@@ -0,0 +1,9 @@
+insert into `estados` (id, nome, sigla, regiao, populacao)
+values (1000, 'Novo', 'NV', 'Sul', 2.54)
+
+select * from `estados`
+
+insert into estados (nome, sigla, regiao, populacao)
+values ('Mais novo', 'MN', 'Norte', 2.99)
+
+select * from  `estados`
