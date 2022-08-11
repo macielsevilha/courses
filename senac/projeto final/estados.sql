71480db378d2fcf285bd8ac4d7cc61cd28ded8d6
MacielSevilha <dev.macielsevilha@gmail.com>
2022-08-11 19:36:21 -0300

    Criar Tabela Prefeitos

---
+++ senac/projeto final/estados.sql
@@ -0,0 +1,12 @@
+CREATE TABLE centroOeste(
+    id_ot INT PRIMARY KEY AUTO_INCREMENT,
+    data_ot DATE NOT NULL,
+    estados_ot CHAR(2) NOT NULL,
+    numCasos_ot NUMERIC NOT NULL,
+    numRecuperados_ot NUMERIC NOT NULL,
+    numMortes_ot NUMERIC NOT NULL,
+    numVacinados1_ot NUMERIC NOT NULL,
+    numVacinados2_ot NUMERIC NOT NULL,
+    numVacinados3_ot NUMERIC NOT NULL,
+
+)
