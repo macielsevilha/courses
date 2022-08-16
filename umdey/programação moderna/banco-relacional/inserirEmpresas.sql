22bee1f3e1120c417f7ff249ee686aa60121be28
MacielSevilha <dev.macielsevilha@gmail.com>
2022-08-16 18:32:13 -0300

    comandos Básicos

--- umdey/programação moderna/banco-relacional/inserirEmpresas.sql
+++ umdey/programação moderna/banco-relacional/inserirEmpresas.sql
@@ -5,3 +5,12 @@ INSERT INTO empresas(nome, cnpj)
 VALUES ('Bradesco', 95694186000131),
        ('Vale', 27887148000146),
        ('Cielo', 01598317000134);
+
+DESC empresas;
+SELECT * FROM empresas;
+
+INSERT INTO empresas_unidades(empresa_id, cidade_id, sede)
+VALUES (1,1,1),
+       (1,2,0),
+       (2,1,0),
+       (2,2,1);
---
+++ umdey/programação moderna/banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,15 @@
+select * from `cidades`;
+
+INSERT INTO prefeitos
+    (nome, cidade_id) 
+VALUES 
+    ('Rodrigo Neves', 2),
+    ('Raquel, Lyra', 3),
+    ('Zenaldo Coutinho', null);
+
+select * from prefeitos;
+
+INSERT INTO prefeitos (nome, cidade_id)
+VALUES ('Rafael Greca', null)
+
+select * from prefeitos;
