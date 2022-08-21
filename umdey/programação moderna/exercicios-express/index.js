f188f2f492f1e9b879afa202ea34e6d623be2034
Maciel <dev.macielsevilha@gmail.com>
2022-08-21 20:50:24 -0300

    Tratando a Primeira Requisisão

--- umdey/programação moderna/exercicios-express/index.js
+++ umdey/programação moderna/exercicios-express/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use('/opa',(req, res) => {
+    res.send('Estou <b>bem!</b>')
+})
+
 app.listen(3000, () => {
     console.log('Backend funcionando...')
 })
