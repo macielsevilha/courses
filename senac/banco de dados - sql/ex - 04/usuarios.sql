a40dcf0457d3b263cd870b7e8faa34e029edd821
Maciel <dev.macielsevilha@gmail.com>
2022-08-27 09:51:57 -0300

    Configurar Projeto

---
+++ senac/banco de dados - sql/ex - 04/usuarios.sql
@@ -0,0 +1,16 @@
+
+CREATE USER 'ester.silva'@'localhost' IDENTIFIED BY 'user0953';
+CREATE USER 'maciel.botelho'@'%' IDENTIFIED BY 'user0953';
+CREATE USER 'lucas.botelho'@'localhost' IDENTIFIED BY 'user0953';
+CREATE USER 'ceilia.cardoso'@'localhost' IDENTIFIED BY 'user0953';
+CREATE USER 'pietro.ferreira'@'%' IDENTIFIED BY 'user0953';
+CREATE USER 'victor.sevilha'@'localhost' IDENTIFIED BY 'user0953';
+CREATE USER 'ana.migues'@'%' IDENTIFIED BY 'user0953';
+CREATE USER 'sofia.melo'@'localhost' IDENTIFIED BY 'user0953';
+CREATE USER 'julia.silva'@'localhost' IDENTIFIED BY 'user0953';
+CREATE USER 'marcos.oliveira'@'%' IDENTIFIED BY 'user0953';
+
+
+/* PARA EXERCUTAR VAI NA OPÇÃO ADMINISTRATION - USERS AND PRIVIL */
+/* NÃO APAGA O ROOT NEM USER POIS PERDE O ACESSO TOTAL AO BD */
+
