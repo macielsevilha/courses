7461704ec2a3fa752feaefc305ab23398c3f37f7
Maciel <dev.macielsevilha@gmail.com>
2022-09-05 01:40:14 -0300

    resolvi o erro na versão do pg

--- umdey/programação moderna/knowledge/versao-inicial/backend/migrations/20220903003249_create_table_users.js
+++ umdey/programação moderna/knowledge/versao-inicial/backend/migrations/20220903003249_create_table_users.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('users', table => {
+        table.increments('id').primary()
+        table.string('nome').notNull()
+        table.string('email').notNull().unique()
+        table.string('password').notNull()
+        table.string('admin').notNull().defaultTo(false)
+      })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('users')
 };
