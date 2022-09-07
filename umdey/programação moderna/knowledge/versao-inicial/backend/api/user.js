a79b56a4e340028903b0f98cbc17c661a774fab0
Maciel <dev.macielsevilha@gmail.com>
2022-09-06 22:03:20 -0300

    error API de Usuário

--- umdey/programação moderna/knowledge/versao-inicial/backend/api/user.js
+++ umdey/programação moderna/knowledge/versao-inicial/backend/api/user.js
@@ -1,7 +1,51 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('user save')
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
+    const save = async (req, res) => {
+         const user = { ...req.body }
+         if(req.params.id) user.id = req.params.id
+
+         try {
+            existsOrError(user.name, 'Nome não informado')
+            existsOrError(user.email, 'E-mail não informado')
+            existsOrError(user.password, 'Senha não informado')
+            existsOrError(user.confirmPassword, 'Confirmação de Senha inválida')
+            equalsOrError(user.password, user.confirmPassword, 
+                'Senhas não conferem')
+            
+            const userFromDB = await app.db('users')
+               .where({ email: user.email}).first()
+            if(!user.id) {
+                notExistsOrError(userFromDB, 'Usuário já cadastrado')
+            }
+         } catch(msg) {
+            return res.status(400).send(msg)
+         }
+
+         user.password = encryptPassword(user.password)
+         delete user.confirmPassword 
+
+         if(user.id) {
+            app.db('users')
+              .update(user)
+              .where({ id: user.id})
+              .then(_ => res.status(204).send())
+              .catch(err => res.status(500).send(err))
+         }
     }
 
-    return { save }
+    const get = (req, res) => {
+        app.db('users')
+          .select('id', 'name', 'email', 'admin')
+          .then(users => res.json(users))
+          .catch(err => res.status(500).send(err))
+    }
+
+    return { save, get }
 }
