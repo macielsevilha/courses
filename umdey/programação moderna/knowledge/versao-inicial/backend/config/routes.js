7fafc3ce4a2684de77815134b24131c616901858
Maciel <dev.macielsevilha@gmail.com>
2022-09-10 10:33:20 -0300

    API de Categoria #05

--- umdey/programação moderna/knowledge/versao-inicial/backend/config/routes.js
+++ umdey/programação moderna/knowledge/versao-inicial/backend/config/routes.js
@@ -11,8 +11,13 @@ module.exports = app => {
       .get(app.api.category.get)
       .post(app.api.category.save)
 
+   // Cuidado com a ordem! tem que vir antes de /categories/:id
+   app.route('/categories/tree')
+      .get(app.api.category.getTree)
+      
    app.route('/categories/:id')
       .get(app.api.category.getById)
       .put(app.api.category.save)
       .delete(app.api.category.remove)
+
 }
