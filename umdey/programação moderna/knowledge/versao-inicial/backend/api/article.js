13514c7e98f11754820838fe2b340a9dfc694134
Maciel <dev.macielsevilha@gmail.com>
2022-09-12 20:55:13 -0300

    API de Artigo #03

--- umdey/programação moderna/knowledge/versao-inicial/backend/api/article.js
+++ umdey/programação moderna/knowledge/versao-inicial/backend/api/article.js
@@ -1,9 +1,9 @@
 module.exports = app => {
-    const { existsOrError, notExistsOrError } = app.api.validation
+    const { existsOrError } = app.api.validation
 
     const save = (req, res) => {
         const article = { ...req.body }
-        if(req.params.id) article.id = req.params.id
+        if (req.params.id) article.id = req.params.id
 
         try {
             existsOrError(article.name, 'Nome não informado')
@@ -11,21 +11,21 @@ module.exports = app => {
             existsOrError(article.categoryId, 'Categoria não informada')
             existsOrError(article.userId, 'Autor não informado')
             existsOrError(article.content, 'Conteúdo não informado')
-        } catch(msg) {
+        } catch (msg) {
             res.status(400).send(msg)
         }
 
-        if(article.id) {
+        if (article.id) {
             app.db('articles')
-               .update(article)
-               .where({ id: article.id })
-               .then(_ => res.status(204).send())
-               .catch(err => res.status(500).send(err))
+                .update(article)
+                .where({ id: article.id })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
         } else {
             app.db('articles')
-               .insert(article)
-               .then(_ => res.status(204).send())
-               .catch(err => res.status(500).send(err))
+                .insert(article)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
         }
 
     }
@@ -33,26 +33,47 @@ module.exports = app => {
     const remove = async (req, res) => {
         try {
             const rowsDeleted = await app.db('articles')
-                .where({ id: req.params.id}).del()
-            notExistsOrError(rowsDeleted, 'Artigo não foi encontrado.')
+                .where({ id: req.params.id }).del()
+            
+                try {
+                    ExistsOrError(rowsDeleted, 'Artigo não foi encontrado.')
 
+                } catch(msg) {
+                    return  res.status(400).send(msg)
+                }
+          
             res.status(204).send()
-        } catch(msg) {
+        } catch (msg) {
             res.status(500).send(msg)
         }
     }
+
     const limit = 10 // usado para paginação
-    const get  = async (req, res) => {
+    const get = async (req, res) => {
         const page = req.query.page || 1
 
         const result = await app.db('articles').count('id').first()
         const count = parseInt(result.count)
 
         app.db('articles')
-           .select('id', 'name', 'description')
-           .limit(limit).offset(page * limit - limit)
-           .then(article => res.json({ data: article, count, limit}))
-           .catch(err => res.status(500).send(err))
+            .select('id', 'name', 'description')
+            .limit(limit).offset(page * limit - limit)
+            .then(article => res.json({ data: article, count, limit }))
+            .catch(err => res.status(500).send(err))
 
     }
+
+    const getById = (req, res) => {
+        app.db('articles')
+            .where({ id: req.params.id })
+            .first()
+            .then(article => {
+                article.content = article.content.toString()
+                return res.json(article)
+            })
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, remove, get , getById }
 }
+
