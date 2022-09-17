b2fb0bda775b03d25b91b4e7f2125fb36a2bd99b
Maciel <dev.macielsevilha@gmail.com>
2022-09-17 09:41:56 -0300

    Estrutura Inicial do Template

--- umdey/programação moderna/knowledge/versao-inicial/frontend/src/App.vue
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/App.vue
@@ -1,15 +1,44 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<header-vue />
+		<menu-vue />
+		<content-vue />
+		<footer-vue />
 	</div>
 </template>
 
 <script>
+import HeaderVue from './assets/components/template/Header.vue';
+import MenuVue from './assets/components/template/Menu.vue';
+import ContentVue from './assets/components/template/Content.vue';
+import FooterVue from './assets/components/template/Footer.vue';
+
 export default {
 	name: "App",
+	components: { HeaderVue, MenuVue, ContentVue, FooterVue }
 }
 </script>
 
 <style>
+  *{
+	font-family: "Lato", sans-serif;
+  }
 
+  body {
+	margin: 0;
+  }
+  #app {
+	-webkit-font-smoothing: antialiased;
+	-moz-osc-font-smothing: grayscale;
+
+	height: 100vh;
+    display: grid;
+	grid-template-rows: 60px 1fr 40px;
+	grid-template-columns: 300px 1fr;
+	grid-template-areas: 
+	    "header header"
+		"menu content"
+		"menu footer";
+	
+  }
 </style>
