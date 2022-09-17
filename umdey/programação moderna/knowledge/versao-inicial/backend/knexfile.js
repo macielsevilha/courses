158a3f35b59a4c5248706803c0a28f7922f53670
Maciel <dev.macielsevilha@gmail.com>
2022-09-16 21:32:06 -0300

    Informações de Conexão no .env

--- umdey/programação moderna/knowledge/versao-inicial/backend/knexfile.js
+++ umdey/programação moderna/knowledge/versao-inicial/backend/knexfile.js
@@ -1,11 +1,8 @@
+const { db } = require('./.env')
 
 module.exports = {
     client: 'postgresql',
-    connection: {
-        database: 'knowledge',
-        user: 'postgres',
-        password: '123456'
-    },
+    connection: db,
     pool: {
         min: 2,
         max: 10
