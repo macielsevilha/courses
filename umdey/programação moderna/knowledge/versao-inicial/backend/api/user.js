d94c6118c610523765581cb77985b9c3427cc8ec
Maciel <dev.macielsevilha@gmail.com>
2022-09-16 21:22:48 -0300

    Soft Delete de Usuário

--- umdey/programação moderna/knowledge/versao-inicial/backend/api/user.js
+++ umdey/programação moderna/knowledge/versao-inicial/backend/api/user.js
@@ -38,7 +38,7 @@ module.exports = app => {
             app.db('users')
               .update(user)
               .where({ id: user.id})
-              //.whereNull('deletedAt')
+              .whereNull('deletedAt')
               .then(_ => res.status(204).send())
               .catch(err => res.status(500).send(err))
          }
@@ -63,11 +63,30 @@ module.exports = app => {
         app.db('users')
         .select('id', 'name', 'email', 'admin')
         .where({ id: req.params.id })
+        .whereNull('deletedAt')
         .first()
         //.whereNotNull('id')
         .then(user => res.json(user))
         .catch(err => res.status(500).send(err))
     }
-    return { save, get, getById }
+
+    const remove = async (req, res) => {
+        try {
+            const articles = await app.db('articles')
+                .where({ userId: req.params.id })
+            notExistsOrError(articles, 'Usuário possui artigos.')
+
+            const rowsUpdate = await app.db('users')
+                .update({deleteAt: new Date()})
+                .where({ id: req.params.id })
+            existsOrError(rowsUpdate, 'Usuário não foi encontrado.')
+
+            res.status(204).send()
+        } catch(msg) {
+            res.status(400).send(msg)
+        }
+    }
+
+    return { save, get, getById, remove }
 }
 
--- umdey/programação moderna/knowledge/versao-inicial/backend/config/routes.js
+++ umdey/programação moderna/knowledge/versao-inicial/backend/config/routes.js
@@ -7,13 +7,14 @@ module.exports = app => {
     
     app.route('/users')
        .all(app.config.passport.authenticate())
-       .post(admin(app.api.user.save))
+       .post(app.api.user.save)
        .get(admin(app.api.user.get))
      
     app.route('/users/:id')
       .all(app.config.passport.authenticate())
       .put(admin(app.api.user.save))
       .get(admin(app.api.user.getById))
+      .delete(admin(app.api.user.remove))
    
    app.route('/categories')
       .all(app.config.passport.authenticate())
