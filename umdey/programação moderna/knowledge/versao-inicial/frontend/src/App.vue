179ffaec9a5b17c979a6fdea161358c3ee0da88f
Maciel <dev.macielsevilha@gmail.com>
2022-09-17 14:01:41 -0300

    Visibilidade do Menu (Toggle) #02

--- umdey/programação moderna/knowledge/versao-inicial/frontend/src/App.vue
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app">  
+	<div id="app" :class="{'hide-menu' : !isMenuVisible}">  
 		<header-vue title="Maciel - Base de conhecimento" :hideToggle="false" />
 		<menu-vue />
 		<content-vue />
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import HeaderVue from './components/template/Header.vue';
 import MenuVue from './components/template/Menu.vue';
 import ContentVue from './components/template/Content.vue';
@@ -15,7 +16,8 @@ import FooterVue from './components/template/Footer.vue';
 
 export default {
 	name: "App",
-	components: { HeaderVue, MenuVue, ContentVue, FooterVue }
+	components: { HeaderVue, MenuVue, ContentVue, FooterVue },
+	computed: mapState(['isMenuVisible'])
 }
 </script>
 
@@ -40,5 +42,11 @@ export default {
 		"menu content"
 		"menu footer";
 	
+  }
+  #app.hide-menu {
+	grid-template-areas: 
+	     "header header"
+		 "content content"
+		 "footer footer";
   }
 </style>
--- umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/template/Header.vue
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/template/Header.vue
@@ -18,7 +18,7 @@ export default {
   },
   computed: {
     icon() {
-        return "fa-angle-left"
+        return this.$store.state.isMenuVisible ? "fa-angle-left" : "fa-angle-down"
     }
   },
   methods: {
