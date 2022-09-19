7c9c82b4065649fea97aa88259c77ec3e2e9481b
Maciel <dev.macielsevilha@gmail.com>
2022-09-19 19:32:37 -0300

    Páginas Administrativas

--- umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -2,15 +2,33 @@
   <div class="admin-pages">
     <page-title-vue icon="fa fa-cogs" main="Administração do Sistema"
        sub="Cadastros & Cia" />
+       <div class="admin-pages-tabs">
+        <b-card no-body>
+           <b-tabs card>
+             <b-tab title="Artigos" active>
+                 <article-admin-vue />
+             </b-tab>
+             <b-tab title="Categorias">
+                <category-admin-vue />
+             </b-tab>
+             <b-tab title="Usuários">
+                <user-admin-vue />
+             </b-tab>
+           </b-tabs>
+        </b-card>
+       </div>
   </div>
 </template>
 
 <script>
 import PageTitleVue from '../template/PageTitle.vue';
+import ArticleAdminVue from './ArticleAdmin.vue';
+import CategoryAdminVue from './CategoryAdmin.vue';
+import UserAdminVue from './UserAdmin.vue';
 
 export default {
   name: "AdminPages",
-  components: { PageTitleVue }
+  components: { PageTitleVue, ArticleAdminVue, CategoryAdminVue, UserAdminVue }
 }
 </script>
 
