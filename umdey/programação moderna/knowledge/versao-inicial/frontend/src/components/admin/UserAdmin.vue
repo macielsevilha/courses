e6c484f178ff3f6602193c1dfa82aec870eedc47
Maciel <dev.macielsevilha@gmail.com>
2022-09-19 19:50:38 -0300

    Cadastro de Usuário #01

--- umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="user-admin">
-    <h1>Usuários Componente</h1>
+    <b-table hover striped :items="users" :fields="fields">
+
+    </b-table>
   </div>
 </template>
 
 <script>
+import { baseApiUrl } from '@/global';
+import Axios from 'axios';
 export default {
-  name: 'UserAdmin'
+  name: 'UserAdmin',
+  data: function() {
+    return {
+        mode: 'save',
+        user: {},
+        users: [],
+        fields: [
+            { key: 'id', label: 'Código', sortable: true}, 
+            { key: 'name', label: 'Nome', sortable: true}, 
+            { key: 'email', label: 'E-mail', sortable: true}, 
+            { key: 'admin', label: 'Administrador', sortable: true, 
+               formatter: value => value ? 'Sim': 'Não'},
+            { key: 'actions', label: 'Ações' } 
+        ]
+    }
+  },
+  methods: {
+    loadUsers() {
+        const url = `${baseApiUrl}/users`
+        Axios.get(url).then(res => {
+            this.users = res.data
+          
+        })
+    }
+  },
+  mounted() {
+    this.loadUsers()
+  }
 }
 </script>
 
--- umdey/programação moderna/knowledge/versao-inicial/frontend/src/main.js
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/main.js
@@ -10,7 +10,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // TEMPORARIO !
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NCwibmFtZSI6Ik1hY2llbCBEIEFuam9zIiwiZW1haWwiOiJtYWNpZWxzZXZpbGhhQGdtYWlsLmNvbSIsImFkbWluIjpmYWxzZSwiaWF0IjoxNjYzNTkzOTY0LCJleHAiOjE2NjM4NTMxNjR9.n_usckPxa0eTroC2GKvlvk37EfmQQJi1Dmkte7bOplU'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6OCwibmFtZSI6ImVzdGVyIiwiZW1haWwiOiJlc3RlcjEyMzQ1NkBnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjYzNjI3MjA2LCJleHAiOjE2NjM4ODY0MDZ9.RUP_Y4G-rEJXvEr8de7T8z_rm-ioBPJVRLlQ0dmDa08'
 
 new Vue({
   store,
