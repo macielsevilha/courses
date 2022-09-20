3ee2e600f10e062bd1016645cdb261464dadc177
Maciel <dev.macielsevilha@gmail.com>
2022-09-20 11:12:08 -0300

    Cadastro de Usuário #02 erro

--- umdey/programação moderna/knowledge/versao-inicial/backend/api/user.js
+++ umdey/programação moderna/knowledge/versao-inicial/backend/api/user.js
@@ -7,67 +7,65 @@ module.exports = app => {
         const salt = bcrypt.genSaltSync(10)
         return bcrypt.hashSync(password, salt)
     }
+
     const save = async (req, res) => {
         const user = { ...req.body }
         if(req.params.id) user.id = req.params.id
 
-       // if(!req.originalUrl.startsWith('/users')) user.admin = false
-       // if(!req.user || !req.user.admin) user.admin = false
-         
-         try {
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
+        try {
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'E-mail não informado')
-            existsOrError(user.password, 'Senha não informado')
+            existsOrError(user.password, 'Senha não informada')
             existsOrError(user.confirmPassword, 'Confirmação de Senha inválida')
-            equalsOrError(user.password, user.confirmPassword, 
+            equalsOrError(user.password, user.confirmPassword,
                 'Senhas não conferem')
-            
+
             const userFromDB = await app.db('users')
-               .where({ email: user.email}).first()
+                .where({ email: user.email }).first()
             if(!user.id) {
                 notExistsOrError(userFromDB, 'Usuário já cadastrado')
             }
-         } catch(msg) {
+        } catch(msg) {
             return res.status(400).send(msg)
-         }
+        }
 
-         user.password = encryptPassword(user.password)
-         delete user.confirmPassword 
+        user.password = encryptPassword(user.password)
+        delete user.confirmPassword
 
-         if(user.id) {
+        if(user.id) {
             app.db('users')
-              .update(user)
-              .where({ id: user.id})
-              .whereNull('deletedAt')
-              .then(_ => res.status(204).send())
-              .catch(err => res.status(500).send(err))
-         }
-         else {
+                .update(user)
+                .where({ id: user.id })
+                .whereNull('deletedAt')
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
             app.db('users')
-               .insert(user)
-               .then(_ => res.status(204).send())
-               .catch(err => res.status(500).send(err))
-         }
+                .insert(user)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
     }
 
     const get = (req, res) => {
         app.db('users')
-        .select('id', 'name', 'email', 'admin')
-        .whereNull('deletedAt')
-        //.whereNotNull('id')
-        .then(users => res.json(users))
-        .catch(err => res.status(500).send(err))
+            .select('id', 'name', 'email', 'admin')
+            .whereNull('deletedAt')
+            .then(users => res.json(users))
+            .catch(err => res.status(500).send(err))
     }
-    
+
     const getById = (req, res) => {
         app.db('users')
-        .select('id', 'name', 'email', 'admin')
-        .where({ id: req.params.id })
-        .whereNull('deletedAt')
-        .first()
-        //.whereNotNull('id')
-        .then(user => res.json(user))
-        .catch(err => res.status(500).send(err))
+            .select('id', 'name', 'email', 'admin')
+            .where({ id: req.params.id })
+            .whereNull('deletedAt')
+            .first()
+            .then(user => res.json(user))
+            .catch(err => res.status(500).send(err))
     }
 
     const remove = async (req, res) => {
@@ -76,10 +74,10 @@ module.exports = app => {
                 .where({ userId: req.params.id })
             notExistsOrError(articles, 'Usuário possui artigos.')
 
-            const rowsUpdate = await app.db('users')
-                .update({deleteAt: new Date()})
+            const rowsUpdated = await app.db('users')
+                .update({deletedAt: new Date()})
                 .where({ id: req.params.id })
-            existsOrError(rowsUpdate, 'Usuário não foi encontrado.')
+            existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
 
             res.status(204).send()
         } catch(msg) {
@@ -89,4 +87,3 @@ module.exports = app => {
 
     return { save, get, getById, remove }
 }
-
--- umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,13 +1,56 @@
 <template>
   <div class="user-admin">
-    <b-table hover striped :items="users" :fields="fields">
+    <b-form>
+      <input id="user-id" type="hidden" v-model="user.id" />
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form label="Nome:" label-for="user-name">
+             <b-form-input id="user-name" type="text"
+                v-model="user.name" required placeholder="Informe o Nome do Usuário...">
 
-    </b-table>
+             </b-form-input>
+          </b-form>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form label="E-mail:" label-for="user-email">
+             <b-form-input id="user-email" type="text"
+                v-model="user.email" required placeholder="Informe o E-mail do Usuário...">
+
+             </b-form-input>
+          </b-form>
+        </b-col>
+      </b-row>
+      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">
+        Administrador?
+      </b-form-checkbox>
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form label="Senha:" label-for="user-password">
+             <b-form-input id="user-password" type="password"
+                v-model="user.password" required placeholder="Informe a Senha do Usuário...">
+             </b-form-input>
+          </b-form>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form label="Condirmaação de Senha:" label-for="user-confirmPassword">
+             <b-form-input id="user-confirmPassword" type="password"
+                v-model="user.confirmPassword" required placeholder="Confirme a Senha do Usuário...">
+
+             </b-form-input>
+          </b-form>
+        </b-col>
+      </b-row>
+      <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
+      <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
+      <b-button class="m-2" @click="reset">Cancelar</b-button>
+    </b-form>
+    <hr />
+    <b-table hover striped :items="users" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
-import { baseApiUrl } from '@/global';
+import { baseApiUrl, showError } from '@/global';
 import Axios from 'axios';
 export default {
   name: 'UserAdmin',
@@ -33,6 +76,30 @@ export default {
             this.users = res.data
           
         })
+    },
+    reset() {
+      this.mode = 'save'
+      this.user = {} 
+      this.loadUsers()
+    },
+    save() {
+      const method = this.user.id ? 'put' : 'post'
+      const id = this.user.id ? `/${this.user.id}` : ''
+      Axios[method](`${baseApiUrl}/users${id}`, this.user)
+         .then(() => {
+          this.$toasted.global.defaultSuccess()
+          this.reset()
+         })
+         .catch(showError)
+    },
+    remove() {
+      const id =this.user.id
+      Axios.delete(`${baseApiUrl}/users/${id}`)
+         .then(() => {
+          this.$toasted.global.defaultSuccess()
+          this.reset()
+         })
+         .catch(showError)
     }
   },
   mounted() {
