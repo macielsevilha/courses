c80c3848728c68d24e67924c85719378fee0363c
Maciel <dev.macielsevilha@gmail.com>
2022-09-23 10:16:30 -0300

    Artigos Por Categoria #01

---
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="articles-by-category">
+      <page-title-vue icon="fa fa-folder-o" :main="category.name" sub="Categoria" />
+    </div>
+  </template>
+  
+  <script>
+  import { baseApiUrl } from '../../global';
+  import Axios from 'axios';
+  import PageTitleVue from '../template/PageTitle.vue';
+  
+  export default {
+     name: 'ArticleByCategory',
+     components: { PageTitleVue },
+     data: function() {
+      return {
+          category: {},
+          articles: [],
+          page: 1,
+          loadMore: true
+      }
+     },
+     methods: {
+      getCategory() {
+          const url = `${baseApiUrl}/categories/${this.category.id}`
+          Axios(url).then(res => this.category = res.data)
+      }
+     },
+     mounted() {
+       this.category.id = this.$route.params.id
+       this.getCategory()
+     }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
