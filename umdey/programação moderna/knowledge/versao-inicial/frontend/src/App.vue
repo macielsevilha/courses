b8940cfdd4f7183fc4585175a7ef3df21a8c8380
Maciel <dev.macielsevilha@gmail.com>
2022-09-24 20:43:40 -0300

    Autenticação: Validar Token #01

--- umdey/programação moderna/knowledge/versao-inicial/frontend/src/App.vue
+++ umdey/programação moderna/knowledge/versao-inicial/frontend/src/App.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import Axios from 'axios';
+import { baseApiUrl, userKey } from './global';
 import { mapState } from 'vuex';
 import HeaderVue from './components/template/Header.vue';
 import MenuVue from './components/template/Menu.vue';
@@ -19,7 +21,42 @@ import FooterVue from './components/template/Footer.vue';
 export default {
 	name: "App",
 	components: { HeaderVue, MenuVue, ContentVue, FooterVue },
-	computed: mapState(['isMenuVisible', 'user'])
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return {
+			validateToken: true
+		}
+	},
+	methods: {
+		async validateToken() {
+			this.validateToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData) {
+				this.validateToken = false
+				this.$router.push({ name: 'auth' })
+				return 
+			}
+
+			const res = await Axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if(res.data) {
+				this.$store.commit('setUser', userData)
+			} else {
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+
+			this.validateToken = false
+
+		}
+	},
+	created() {
+		this.validateToken()
+	}
 }
 </script>
 
