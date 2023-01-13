f8496901c1e3c2a6edf8f44c676b829ff0aeeb6e
Maciel <dev.macielsevilha@gmail.com>
2023-01-13 11:05:12 -0300

    App Orçamento Pessoal - Criando a classe Despesa

--- umdey/desenvolvimento Web/ES6/app1/app.js
+++ umdey/desenvolvimento Web/ES6/app1/app.js
@@ -1,4 +1,34 @@
 
 function cadastrarDespesa() {
-    alert()
+
+    class Despesa {
+        constructor(ano, mes, dia, tipo, descricao, valor) {
+            this.ano = ano
+            this.mes = mes
+            this.dia = dia
+            this.tipo = tipo
+            this.descricao = descricao
+            this.valor = valor
+        }
+    }
+    
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+    let despesa = new Despesa(
+        ano.value, 
+        mes.value, 
+        dia.value,
+        tipo.value,
+        descricao.value, 
+        valor.value
+    )
+
+    console.log(despesa)
+   
+
 }
