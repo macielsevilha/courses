27deea7b27b33d71f511d542c6fdd82c61ea280f
Maciel <dev.macielsevilha@gmail.com>
2023-01-13 14:14:16 -0300

    App Orçamento Pessoal - Criando a classe BD e índices dinâmico

--- umdey/desenvolvimento Web/ES6/app1/app.js
+++ umdey/desenvolvimento Web/ES6/app1/app.js
@@ -1,17 +1,45 @@
 
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+class Bd {
+
+    constructor() {
+      let id = localStorage.getItem('id')
+      
+      if(id === null) {
+        localStorage.setItem('id', 0)
+      }
+    }
+
+    getProximoId() {
+       let proximoId = localStorage.getItem('id')
+       return parseInt(proximoId) + 1
+
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
 
-    class Despesa {
-        constructor(ano, mes, dia, tipo, descricao, valor) {
-            this.ano = ano
-            this.mes = mes
-            this.dia = dia
-            this.tipo = tipo
-            this.descricao = descricao
-            this.valor = valor
-        }
-    }
-    
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
     let dia = document.getElementById('dia')
@@ -20,19 +48,15 @@ function cadastrarDespesa() {
     let valor = document.getElementById('valor')
 
     let despesa = new Despesa(
-        ano.value, 
-        mes.value, 
+        ano.value,
+        mes.value,
         dia.value,
         tipo.value,
-        descricao.value, 
+        descricao.value,
         valor.value
     )
 
-    gravar(despesa)
+    bd.gravar(despesa)
 
-   function gravar(d) {
-     localStorage.setItem('despesa', JSON.stringify(d))
-   }
-   
 
 }
