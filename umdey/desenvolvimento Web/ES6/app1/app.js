6e21926c05127bd663f0411f189706dd4618f774
Maciel <dev.macielsevilha@gmail.com>
2023-01-15 23:39:55 -0300

    App Orçamento Pessoal - Listando despesas parte 2

--- umdey/desenvolvimento Web/ES6/app1/app.js
+++ umdey/desenvolvimento Web/ES6/app1/app.js
@@ -118,6 +118,42 @@ function carregaListaDespesas() {
 
    despesas = bd.recuperarTodosOsRegistros()
 
-   console.log(despesas)
+   let listaDespesas = document.getElementById('listaDespesas')
+
+        // < tr >
+        //     <td>10/03/2012</td>
+        //     <td>Alimentação</td>
+        //     <td>Compras do mês</td>
+        //     <td>44.34</td>
+        // </tr >
+
+        despesas.forEach(function(d) {
+
+            let linha = listaDespesas.insertRow() //criando as linhas tr
+            
+            //criando as colunas td
+            linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+            //ajustar tipo
+            switch (parseInt(d.tipo)) {
+                case 1: d.tipo = 'Alimentação'
+                    break;
+                case 2: d.tipo = 'Educação'
+                    break
+                case 3: d.tipo = 'Lazer'
+                    break
+                case 4: d.tipo = 'Saúde'
+                    break
+                case 5: d.tipo = 'Transporte'
+                    break;
+            }
+
+            linha.insertCell(1).innerHTML = d.tipo
+
+            linha.insertCell(2).innerHTML = d.descricao
+            linha.insertCell(3).innerHTML = d.valor
+            
+
+        })
 
 }
