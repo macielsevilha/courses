0a090aedcffae96ae99726087358e40a63d9491b
Maciel <dev.macielsevilha@gmail.com>
2023-01-18 19:53:41 -0300

    C:\xampp\htdocs\echo_print.php

---
+++ umdey/desenvolvimento Web/Php/echo_print.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    //echo é o construtor da linguagem
+      echo 'Comando echo <br>';
+
+    // print é uma função que retorna um valor 0 ou 1
+      print('Comando print <hr>');
+
+      echo print('')
+
+     
+    
+    ?>
+  
+</body>
+</html>
