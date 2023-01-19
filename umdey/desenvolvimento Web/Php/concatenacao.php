7aec3de593edaa1bdc8b979d10f76685c0494f36
Maciel <dev.macielsevilha@gmail.com>
2023-01-19 19:47:47 -0300

    Concatenação

---
+++ umdey/desenvolvimento Web/Php/concatenacao.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+
+//operador .
+    $nome = 'Maciel';
+
+    echo 'Hi ' . $nome . ' How are you?';
+
+    print '<br/>';
+
+    //aspas duplas 
+    echo "Olá $nome como vai?"
+    
+    ?>
+</body>
+</html>
