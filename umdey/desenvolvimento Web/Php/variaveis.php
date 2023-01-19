6f25607130f33908fcfdad85fec72bb4fb219d8f
Maciel <dev.macielsevilha@gmail.com>
2023-01-19 19:39:25 -0300

    Variáveis - Prática (string, int, float e boolean

---
+++ umdey/desenvolvimento Web/Php/variaveis.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+  <?
+   
+    //string
+    $nome = 'Jorge Sant Ana';
+    
+    #int
+    $idade = 20;
+
+    #float
+    $peso = 12.34;
+
+    //boolean
+    $fumante = false;
+  ?>
+
+<h1>Ficha cadastral</h1>
+<p>nome: <?= $nome ?></p>
+<p>nome: <?= $idade ?></p>
+<p>nome: <?= $peso ?></p>
+<p>nome: <?= $fumante ?></p>
+
+</body>
+</html>
