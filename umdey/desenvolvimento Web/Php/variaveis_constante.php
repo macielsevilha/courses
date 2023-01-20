7b8f7b75519a0f69983091e1c988ec2a7603f928
Maciel <dev.macielsevilha@gmail.com>
2023-01-20 15:07:36 -0300

    Variáveis constantes

---
+++ umdey/desenvolvimento Web/Php/variaveis_constante.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+     define('D_NOME', 'Maciel');
+     define('D_EMAIl', 'Maciel@gmail.com');
+     define('D_SENHA', 'appSenha');
+
+     echo D_NOME . ' ' . D_EMAIl . ' ' . D_SENHA
+     
+    ?>
+</body>
+</html>
