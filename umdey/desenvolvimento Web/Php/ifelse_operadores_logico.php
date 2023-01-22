026217040761cbb3bd9343a06fb85d3d68ecf103
Maciel <dev.macielsevilha@gmail.com>
2023-01-22 20:58:51 -0300

    If/else parte 3 - praticando com operadores lógico

---
+++ umdey/desenvolvimento Web/Php/ifelse_operadores_logico.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?
+
+// XOR -> retorna true se uma das condicões for verdadeira e a outra falsa
+// ! -> inverte o resultado da expressão
+// () estabelecer precedência
+
+if (!(2 == 2 XOR 2 == 2)) {
+    echo 'Verdadeiro';
+} else {
+    echo 'Falso';
+}
+
+?>
+</body>
+</html>
