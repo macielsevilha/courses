0d507730558dd49c643dbd19b9553d3d442c3d41
Maciel <dev.macielsevilha@gmail.com>
2023-01-23 14:39:14 -0300

    Operador ternário

--- umdey/desenvolvimento Web/Php/ifelse_praticando_mais_um_pouco.php
+++ umdey/desenvolvimento Web/Php/ifelse_praticando_mais_um_pouco.php
@@ -1,54 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Curso PHP</title>
 </head>
-<body>
-    <?
-    
-    $usuario_possui_cartao_loja = true;
-    $valor_compra = 100.50;
 
-    $valor_frete = 200;
-    
+<body>
+    <?php
+    $usuario_possui_cartao_loja = true;
+    $valor_compra = 50;
+
+    $valor_frete = 50;
     $recebeu_desconto_frete = true;
 
-    if ($usuario_possui_cartao_loja && $valor_compra >= 400) {
+    if ($usuario_possui_cartao_loja  && $valor_compra >= 400) {
         $valor_frete = 0;
-    } else if($usuario_possui_cartao_loja && $valor_compra >= 300 ) {
-       $valor_frete = 50;
-    } else if($usuario_possui_cartao_loja && $valor_compra >= 200) {
-        $valor_frete = 100;
-    } else $recebeu_desconto_frete = false;
-    
-    $valor_compra + $valor_frete;
+  
+    } else if ($usuario_possui_cartao_loja  && $valor_compra >= 300) {
+        $valor_frete = 10;
+  
+    } else if ($usuario_possui_cartao_loja  && $valor_compra >= 100) {
+        $valor_frete = 25;
+  
+    } else {
+        //...
+        $recebeu_desconto_frete = false;
+    }
 
     ?>
 
-  <h1>Detalhes de pedido</h1>
+    <h1>Detalhes do pedido</h1>
+    <p>Possui cartão da loja? <?=  $usuario_possui_cartao_loja ? 'SIM' : 'NÃO';  ?>
+        <?php
+        //<condicao> ? true : false
+        // if ($usuario_possui_cartao_loja) {
+        //     echo 'SIM';
+        // } else {
+        //     echo 'NÃO';
+        // }
+        ?>
+    </p>
 
-  <p>Possui cartão da loja?</p>
-  <? 
-    if ($usuario_possui_cartao_loja) {
-        echo 'Sim';
-    } else {
-        echo 'Não';
-    }
-  ?>
+    <p>Valor da compra: <?= $valor_compra ?></p>
 
-  <p>Recebeu desconto do frete</p>
-  <?php
-     if ($recebeu_desconto_frete) {
-       echo 'Sim';
-     } else {
-        echo 'Não';
-     }
-  ?>
+    <p>Recebeu desconto no frete?
+        <?php
+        $teste = $recebeu_desconto_frete ? 'SIM' : 'NÃO';
+        echo $teste;
+        // if ($recebeu_desconto_frete) {
+        //     echo 'SIM';
+        // } else {
+        //     echo 'NÃO';
+        // }
+        ?>
+    </p>
 
-  <p>Valor da compra: <?= $valor_compra ?></p>
+    <p>Valor do frete: <?= $valor_frete ?></p>
 
 </body>
+
 </html>
