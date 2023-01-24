5fae114be85d49f49fe2b55cc60aac024994a517
Maciel <dev.macielsevilha@gmail.com>
2023-01-23 22:02:17 -0300

    Praticando com operadores aritméticos

---
+++ umdey/desenvolvimento Web/Php/operadores_aritmeticos.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+    $num1 = 13;
+    $num2 = 4;
+
+    echo "A soma entre $num1 e $num2 é " . ($num1 + $num2);
+    echo "<br />";
+    echo "A subtracao entre $num1 e $num2 é " . ($num1 - $num2);
+    echo "<br />";
+    echo "A multiplicacão entre $num1 e $num2 é " . ($num1 * $num2);
+    echo "<br />";
+    echo "A divisão entre $num1 e $num2 é " . ($num1 / $num2);
+    echo "<br />";
+    echo "O módulo entre $num1 e $num2 é " . ($num1 % $num2);
+    ?>
+</body>
+
+</html>
