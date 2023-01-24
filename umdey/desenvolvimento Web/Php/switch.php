76e93bcc8d433962235778f00c872f8197311591
Maciel <dev.macielsevilha@gmail.com>
2023-01-23 21:45:53 -0300

    Switch na prática

---
+++ umdey/desenvolvimento Web/Php/switch.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+
+    $variable = 2
+      switch ($variable) {
+        case 1:
+            echo '1'
+            break;
+        case 2:
+            echo '2'
+            break;
+        
+        default:
+            echo 'nâo existe'
+            break;
+      }
+    ?>
+</body>
+</html>
