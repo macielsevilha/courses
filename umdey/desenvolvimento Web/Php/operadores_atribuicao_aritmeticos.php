779ba90b3c399996cab79826e47c2cc132c46412
Maciel <dev.macielsevilha@gmail.com>
2023-01-23 22:06:02 -0300

    Operações aritméticas na atribuição de valores

---
+++ umdey/desenvolvimento Web/Php/operadores_atribuicao_aritmeticos.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+    // =
+    // +, -, *, /, %
+
+    $x = 10;
+    $y = 8;
+    //$x = $x + 5;
+    //$x += 5 ;
+    //$x -= $y;
+    //$x /= $y;
+    //$x *= $y;
+    $x %= $y;
+    echo $x;
+
+    ?>
+</body>
+
+</html>
