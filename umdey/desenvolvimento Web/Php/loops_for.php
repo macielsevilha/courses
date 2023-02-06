a3316dbfa8e59b2992758d973b54ad88adc17f54
Maciel <dev.macielsevilha@gmail.com>
2023-02-06 14:16:07 -0300

    Loops parte 4 - For

---
+++ umdey/desenvolvimento Web/Php/loops_for.php
@@ -0,0 +1,50 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+    /* 
+    $x = 1;
+    while ($x < 10){
+
+    }
+
+    do {
+
+    }while($x <10);
+    */
+
+    // for($x = 1; $x <= 10; $x++){
+    //     echo "$x <br/>";
+    // }
+
+    // for ($x = 11; true; $x++) {
+
+    //     if ($x >= 20) {
+    //         break;
+    //     }
+    //     echo "$x <br/>";
+    // }
+
+    // for ($x = 10; true; $x--) {
+
+    //     if ($x == 0) {
+    //         break;
+    //     }
+    //     echo "$x <br/>";
+    // }
+
+    for ($x = 10; $x > 0; $x--) {
+
+        //break
+        //continue
+        echo "$x <br/>";
+    }
+    ?>
+</body>
+
+</html>
