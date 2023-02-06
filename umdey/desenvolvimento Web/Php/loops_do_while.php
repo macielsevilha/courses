fa6d7aaa4361e38d1a3df1c965623b2b8e9691da
Maciel <dev.macielsevilha@gmail.com>
2023-02-06 14:11:28 -0300

    Loops parte 3 - Do while

---
+++ umdey/desenvolvimento Web/Php/loops_do_while.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+    $x = 1;
+    do {
+        echo "X = $x <br />";
+        $x++; //critério de parada
+        //continue
+        //break
+    } while($x < 9);
+
+    // echo '<br />';
+
+    // while($x < 9 ) {
+    //     echo 'Entrou no while';
+    // }
+    ?>
+</body>
+
+</html>
