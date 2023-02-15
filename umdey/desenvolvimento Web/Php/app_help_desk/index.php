d40c87821d913cbf42cd7dff58d695834a631dea
Maciel <dev.macielsevilha@gmail.com>
2023-02-15 15:27:24 -0300

    App Help Desk - Autenticando usuário

--- umdey/desenvolvimento Web/Php/app_help_desk/index.php
+++ umdey/desenvolvimento Web/Php/app_help_desk/index.php
@@ -1,3 +1,5 @@
+
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -39,6 +41,19 @@
                 <div class="form-group">
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+                <?
+
+                 if(isset($_GET['login']) && $_GET['login'] == 'erro') {
+                 
+                ?>
+                
+                <div class="text-danger">
+                  Usuário ou senha inválida!
+                </div>
+
+
+                <? } ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- umdey/desenvolvimento Web/Php/app_help_desk/valida_login.php
+++ umdey/desenvolvimento Web/Php/app_help_desk/valida_login.php
@@ -1,7 +1,20 @@
 <?php
 
-echo $_POST['email'];
-echo $_POST['senha'];
+$usuario_autenticado = false;
 
+$usuario_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+);
+
+foreach ($usuario_app as $user) {
+    if($user['email'] === $_POST['email'] && $user['senha'] === $_POST['senha']) $usuario_autenticado = true;
+}
+
+if ($usuario_autenticado) {
+   echo 'Usuário logado!';
+} else {
+    header('Location: index.php?login=erro');
+}
 
 ?>
